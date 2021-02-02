44397afa944f4a85edd23c6b42488ce28d03ea98
avinashvintek <avinash@vinteksolution.com>
2021-02-02 16:34:54 +0530

    Google Sign in

--- app/screens/AuthScreen.js
+++ app/screens/AuthScreen.js
@@ -1,5 +1,10 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import * as Google from 'expo-google-app-auth'
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin'
 import { StyleSheet, View, Text } from 'react-native'
 
 import AppButton from '../components/AppButton'
@@ -13,8 +18,13 @@ import usersApi from '../api/users'
 
 const config = {
   // iosClientId: `<YOUR_IOS_CLIENT_ID>`,
+  // androidClientId:
+  //   '320113619885-drs735a38tcvfq000k0psg7t60c8nfff.apps.googleusercontent.com',
   androidClientId:
     '320113619885-drs735a38tcvfq000k0psg7t60c8nfff.apps.googleusercontent.com',
+  // androidStandaloneAppClientId:
+  //   '320113619885-drs735a38tcvfq000k0psg7t60c8nfff.apps.googleusercontent.com',
+
   scopes: ['profile', 'email'],
 }
 
@@ -57,6 +67,53 @@ const AuthScreen = ({ route, navigation }) => {
     }
   }
 
+  // useEffect(() => {
+
+  // }, [])
+
+  const signIn = async () => {
+    await GoogleSignin.configure({
+      // webClientId:
+      //   '320113619885-7ne0jghh8ogpk43h8u746djtq9h3ls23.apps.googleusercontent.com',
+      androidClientId:
+        '320113619885-drs735a38tcvfq000k0psg7t60c8nfff.apps.googleusercontent.com',
+    })
+    try {
+      setLoading(true)
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      console.log('User', userInfo.user)
+      const password = userInfo.user.id + Date.now()
+      const res = await authApi.saveGoogleUser(
+        userInfo.user.name,
+        userInfo.user.email,
+        password
+      )
+      if (!res.ok) {
+        setLoading(false)
+        // setError(res.data.msg);
+        console.log(res)
+        return
+      }
+      authStorage.storeToken(res.data.token)
+      const userRes = await usersApi.getLoggedInUser()
+      setUser(userRes.data.user)
+
+      setLoading(false)
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('e 1')
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('e 2')
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('e 3')
+      } else {
+        console.log('Eror', error)
+      }
+      setLoading(false)
+    }
+  }
+
   return (
     <>
       <LoadingIndicator visible={loading} />
@@ -68,7 +125,15 @@ const AuthScreen = ({ route, navigation }) => {
 
         <AppButton title='Continue with Email ID' onPress={handlePress} />
 
-        <AppButton title='Continue with Google' onPress={signInWithGoogle} />
+        <AppButton title='Continue with Google' onPress={signIn} />
+
+        {/* <GoogleSigninButton
+          style={styles.googleBtn}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signIn}
+          // disabled={loading}
+        /> */}
       </View>
     </>
   )
@@ -87,6 +152,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 22,
     alignSelf: 'center',
+  },
+  googleBtn: {
+    width: '100%',
+    // backgroundColor: 'red',
   },
 })
 
