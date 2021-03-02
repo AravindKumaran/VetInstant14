811c073206faa188f639fcfbf0d9ffee878d652b
Tamilvanan25 <tamilvanan@vinteksolution.com>
2021-03-02 11:34:42 +0530

    soc

--- app/components/utils/socket.js
+++ app/components/utils/socket.js
@@ -1,11 +1,6 @@
 import IO from 'socket.io-client'
 
-<<<<<<< HEAD
 // const socket = IO('http://192.168.29.239:8000')
 const socket = IO('https://vetinstantbe.azurewebsites.net')
-=======
-const socket = IO('http://192.168.43.242:8000')
-// const socket = IO('https://vetinstantbe.azurewebsites.net')
->>>>>>> 8c8b9ca29c12ab00c8408e0ead22c5761976297d
 
 export default socket
