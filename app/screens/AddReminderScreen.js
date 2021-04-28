e43c20605d0e43190bf3bfeece2aadfa569f6d97
Tamilvanan25 <tamilvanan@vinteksolution.com>
2021-04-28 17:29:08 +0530

    Reminder Screen UI Changes

--- app/screens/AddReminderScreen.js
+++ app/screens/AddReminderScreen.js
@@ -1,148 +1,148 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   ScrollView,
   StyleSheet,
   View,
   Platform,
   TouchableOpacity,
-} from 'react-native'
-import DateTimePicker from '@react-native-community/datetimepicker'
-import { Feather } from '@expo/vector-icons'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
-import * as Notifications from 'expo-notifications'
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Feather } from "@expo/vector-icons";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import * as Notifications from "expo-notifications";
 
-import AppFormPicker from '../components/forms/AppFormPicker'
-import AppFormField from '../components/forms/AppFormField'
-import AppText from '../components/AppText'
-import SubmitButton from '../components/forms/SubmitButton'
-import { storeObjectData } from '../components/utils/reminderStorage'
+import AppFormPicker from "../components/forms/AppFormPicker";
+import AppFormField from "../components/forms/AppFormField";
+import AppText from "../components/AppText";
+import SubmitButton from "../components/forms/SubmitButton";
+import { storeObjectData } from "../components/utils/reminderStorage";
 
 const reminders = [
-  { label: 'Deworming', value: 'Deworming' },
-  { label: 'Medicine', value: 'Medicine' },
-  { label: 'Vaccine', value: 'Vaccine' },
-]
+  { label: "Deworming", value: "Deworming" },
+  { label: "Medicine", value: "Medicine" },
+  { label: "Vaccine", value: "Vaccine" },
+];
 
 const validationSchema = Yup.object().shape({
-  reminder: Yup.string().nullable().required('Please select a reminder'),
-  notes: Yup.string().max(50, 'Notes must be less than 50 Characters'),
+  reminder: Yup.string().nullable().required("Please select a reminder"),
+  notes: Yup.string().max(50, "Notes must be less than 50 Characters"),
   // mdname: Yup.string().required().label('Medicine name'),
-})
+});
 
 const getTom = () => {
-  const today = new Date()
-  today.setSeconds(0)
+  const today = new Date();
+  today.setSeconds(0);
   // const tom = new Date(today)
   // tom.setDate(tom.getDate() + 1)
-  return today
-}
+  return today;
+};
 
 const AddReminderScreen = ({ navigation, route }) => {
-  const [date, setDate] = useState(getTom())
-  const [endDate, setEndDate] = useState(getTom())
-  const [isEndDate, setIsEndDate] = useState(false)
-  const [mode, setMode] = useState('date')
-  const [show, setShow] = useState(false)
+  const [date, setDate] = useState(getTom());
+  const [endDate, setEndDate] = useState(getTom());
+  const [isEndDate, setIsEndDate] = useState(false);
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
 
   const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date
-    setShow(Platform.OS === 'ios')
-    setDate(currentDate)
-  }
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+  };
 
   const handleEndDate = (event, selectedDate) => {
-    const currentDate = selectedDate || endDate
-    setIsEndDate(Platform.OS === 'ios')
-    setEndDate(currentDate)
-  }
+    const currentDate = selectedDate || endDate;
+    setIsEndDate(Platform.OS === "ios");
+    setEndDate(currentDate);
+  };
 
   const showEndDate = () => {
-    setIsEndDate(true)
-  }
+    setIsEndDate(true);
+  };
 
   const showMode = (currentMode) => {
-    setShow(true)
-    setMode(currentMode)
-  }
+    setShow(true);
+    setMode(currentMode);
+  };
 
   const showDatepicker = () => {
-    showMode('date')
-  }
+    showMode("date");
+  };
 
   const showTimepicker = () => {
-    showMode('time')
-  }
+    showMode("time");
+  };
 
   const scheduleNotification = async (rmr) => {
-    let trObj = {}
-    if (rmr.reminder === 'Medicine') {
+    let trObj = {};
+    if (rmr.reminder === "Medicine") {
       trObj = {
         hour: 10,
         minute: 0,
         // seconds: 15,
         repeats: true,
-      }
+      };
     } else {
-      date
+      date;
     }
     const identifier = await Notifications.scheduleNotificationAsync({
       content: {
-        title: 'Your Today Reminders Are Pending',
+        title: "Your Today Reminders Are Pending",
         body: `${rmr.reminder}`,
       },
       trigger: trObj,
-    })
-    return identifier
-  }
+    });
+    return identifier;
+  };
 
   const handleSubmit = async (values) => {
-    if (values.reminder !== 'Medicine') {
+    if (values.reminder !== "Medicine") {
       const rmr1 = {
         date,
         ...values,
-      }
-      const idt1 = await scheduleNotification(rmr1)
-      rmr1['identifier'] = idt1
+      };
+      const idt1 = await scheduleNotification(rmr1);
+      rmr1["identifier"] = idt1;
       storeObjectData(
         `${date.toLocaleDateString()}-${date.toLocaleTimeString()}`,
         rmr1
-      )
+      );
 
-      const yesterday = new Date()
-      yesterday.setDate(date.getDate() - 1)
-      yesterday.setHours(12)
-      yesterday.setSeconds(0)
-      yesterday.setMinutes(0)
+      const yesterday = new Date();
+      yesterday.setDate(date.getDate() - 1);
+      yesterday.setHours(12);
+      yesterday.setSeconds(0);
+      yesterday.setMinutes(0);
 
       const rmr2 = {
         yesterday,
         ...values,
-      }
+      };
 
-      const idt2 = await scheduleNotification(rmr2)
+      const idt2 = await scheduleNotification(rmr2);
 
-      rmr2['identifier'] = idt2
+      rmr2["identifier"] = idt2;
       storeObjectData(
         `${yesterday.toLocaleDateString()}-${yesterday.toLocaleTimeString()}`,
         rmr2
-      )
+      );
     } else {
       const rmr1 = {
         date,
         endDate,
         ...values,
-      }
-      const idt1 = await scheduleNotification(rmr1)
-      rmr1['identifier'] = idt1
+      };
+      const idt1 = await scheduleNotification(rmr1);
+      rmr1["identifier"] = idt1;
       storeObjectData(
         `${endDate.toLocaleDateString()}-${endDate.toLocaleTimeString()}`,
         rmr1
-      )
+      );
     }
 
-    navigation.navigate('Home')
-  }
+    navigation.navigate("Home");
+  };
 
   return (
     <ScrollView>
@@ -150,8 +150,8 @@ const AddReminderScreen = ({ navigation, route }) => {
         <Formik
           enableReinitialize
           initialValues={{
-            reminder: '',
-            notes: '',
+            reminder: "",
+            notes: "",
           }}
           onSubmit={handleSubmit}
           validationSchema={validationSchema}
@@ -161,18 +161,18 @@ const AddReminderScreen = ({ navigation, route }) => {
               <>
                 <AppFormPicker
                   items={reminders}
-                  label='Type Of Reminder'
-                  name='reminder'
+                  label="Type Of Reminder"
+                  name="reminder"
                 />
 
-                {values.reminder === 'Medicine' && (
+                {values.reminder === "Medicine" && (
                   <>
                     <AppFormField
-                      label='Medicine Name'
-                      autoCapitalize='none'
+                      label="Medicine Name"
+                      autoCapitalize="none"
                       autoCorrect={false}
-                      name='medicine'
-                      placeholder='Enter medicine name'
+                      name="medicine"
+                      placeholder="Enter medicine name"
                     />
                     <AppText>Choose End Date</AppText>
                     <TouchableOpacity
@@ -180,9 +180,9 @@ const AddReminderScreen = ({ navigation, route }) => {
                       style={styles.dateTime}
                     >
                       <Feather
-                        name='calendar'
+                        name="calendar"
                         size={25}
-                        color='#6e6969'
+                        color="#6e6969"
                         style={styles.icon}
                       />
                       <AppText style={styles.text}>
@@ -191,13 +191,13 @@ const AddReminderScreen = ({ navigation, route }) => {
                     </TouchableOpacity>
                     {isEndDate && (
                       <DateTimePicker
-                        testID='dateTimePicker'
+                        testID="dateTimePicker"
                         value={endDate}
-                        mode='Date'
+                        mode="Date"
                         is24Hour={true}
-                        display='default'
+                        display="default"
                         onChange={handleEndDate}
-                        neutralButtonLabel='clear'
+                        neutralButtonLabel="clear"
                         minimumDate={getTom()}
                       />
                     )}
@@ -211,9 +211,9 @@ const AddReminderScreen = ({ navigation, route }) => {
                   style={styles.dateTime}
                 >
                   <Feather
-                    name='clock'
+                    name="clock"
                     size={25}
-                    color='#6e6969'
+                    color="#6e6969"
                     style={styles.icon}
                   />
                   <AppText style={styles.text}>
@@ -222,7 +222,7 @@ const AddReminderScreen = ({ navigation, route }) => {
                 </TouchableOpacity>
 
                 <AppText
-                  style={{ fontSize: 15, marginBottom: 15, color: '#DC143C' }}
+                  style={{ fontSize: 15, marginBottom: 15, color: "#DC143C" }}
                 >
                   *Time must be greater than current time for successful
                   reminder
@@ -234,9 +234,9 @@ const AddReminderScreen = ({ navigation, route }) => {
                   style={styles.dateTime}
                 >
                   <Feather
-                    name='calendar'
+                    name="calendar"
                     size={25}
-                    color='#6e6969'
+                    color="#6e6969"
                     style={styles.icon}
                   />
                   <AppText style={styles.text}>
@@ -246,35 +246,35 @@ const AddReminderScreen = ({ navigation, route }) => {
 
                 {show && (
                   <DateTimePicker
-                    testID='dateTimePicker'
+                    testID="dateTimePicker"
                     value={date}
                     mode={mode}
                     is24Hour={true}
-                    display='default'
+                    display="default"
                     onChange={onChange}
-                    neutralButtonLabel='clear'
+                    neutralButtonLabel="clear"
                     minimumDate={getTom()}
                   />
                 )}
 
                 <AppFormField
-                  label='Notes (optional)'
-                  autoCapitalize='none'
+                  label="Notes (optional)"
+                  autoCapitalize="none"
                   autoCorrect={false}
-                  name='notes'
+                  name="notes"
                   numberOfLines={4}
-                  placeholder='Max(50) characters.'
+                  placeholder="Max(50) characters."
                 />
 
-                <SubmitButton title='Add Reminder' />
+                <SubmitButton title="Add Reminder" />
               </>
-            )
+            );
           }}
         </Formik>
       </View>
     </ScrollView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -283,10 +283,10 @@ const styles = StyleSheet.create({
     marginVertical: 30,
   },
   dateTime: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: "#fff",
     marginVertical: 10,
     padding: 15,
     borderRadius: 10,
@@ -297,9 +297,9 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 20,
-    color: '#0c0c0c',
+    color: "#0c0c0c",
     flex: 1,
   },
-})
+});
 
-export default AddReminderScreen
+export default AddReminderScreen;
--- app/screens/ReminderScreen.js
+++ app/screens/ReminderScreen.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-import { StyleSheet, ScrollView, View } from "react-native";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import AppButton from "../components/AppButton";
 import AppText from "../components/AppText";
 import { Feather } from "@expo/vector-icons";
@@ -65,14 +71,161 @@ const ReminderScreen = ({ navigation }) => {
         >
           <ChoosePicker items={pet} label="Choose your pet" name="pet" />
         </Formik>
-        <AppButton
-          title="New Reminder"
+        <TouchableOpacity
+          style={{ paddingTop: 30, alignSelf: "flex-end", right: 50 }}
+          onPress={() => refRBSheet.current.open()}
           onPress={() =>
             navigation.navigate("AddReminder", {
               rmr: todayReminders,
             })
           }
-        />
+        >
+          <Feather
+            name={"plus-circle"}
+            size={50}
+            color={"#41CE8A"}
+            style={{
+              backgroundColor: "white",
+              alignSelf: "center",
+              borderRadius: 50,
+            }}
+          />
+        </TouchableOpacity>
+        <View>
+          <View style={{ flexDirection: "row", paddingLeft: 30 }}>
+            <Feather
+              name={"calendar"}
+              size={50}
+              color={"white"}
+              style={{
+                backgroundColor: "#60E6A6",
+                borderRadius: 50,
+                height: 70,
+                width: 70,
+                alignContent: "center",
+              }}
+            />
+            <View style={{ flexDirection: "column" }}>
+              <View
+                style={{
+                  borderColor: "rgba(21, 56, 95, 0.3)",
+                  borderWidth: 1,
+                  marginBottom: 18,
+                  borderRadius: 40,
+                  height: 60,
+                  width: 150,
+                  alignSelf: "center",
+
+                  justifyContent: "center",
+                }}
+              >
+                <AppText
+                  style={{
+                    color: "#47687F",
+                    alignSelf: "center",
+                    fontSize: 14,
+                  }}
+                >
+                  April 11th, 2021
+                </AppText>
+              </View>
+              <View
+                style={{
+                  borderColor: "rgba(21, 56, 95, 0.3)",
+                  borderWidth: 1,
+                  marginBottom: 0,
+                  borderRadius: 40,
+                  height: 50,
+                  width: 250,
+                  alignSelf: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#60E6A6",
+                }}
+              >
+                <AppText
+                  style={{
+                    color: "#ffffff",
+                    alignSelf: "center",
+                    fontSize: 12,
+                    fontWeight: "700",
+                  }}
+                >
+                  Vaccination Day
+                </AppText>
+                <AppText
+                  style={{
+                    color: "#47687F",
+                    alignSelf: "center",
+                    fontSize: 14,
+                    fontWeight: "400",
+                  }}
+                >
+                  Canine Distemper
+                </AppText>
+              </View>
+            </View>
+          </View>
+          <View style={{ flexDirection: "row", alignSelf: "center" }}>
+            <Text
+              style={{
+                color: "#47687F",
+                fontSize: 14,
+                fontWeightL: "400",
+                alignSelf: "center",
+              }}
+            >
+              10:00 am
+            </Text>
+            <View
+              style={{
+                borderColor: "rgba(21, 56, 95, 0.3)",
+                borderWidth: 1,
+                marginBottom: 18,
+                borderRadius: 20,
+                height: 70,
+                width: 250,
+                alignSelf: "center",
+                justifyContent: "center",
+                backgroundColor: "#FFFFFF",
+                margin: 10,
+              }}
+            >
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ flexDirection: "column", marginLeft: 15 }}>
+                  <Text
+                    style={{
+                      color: "#47687F",
+                      fontSize: 14,
+                      fontWeightL: "400",
+                    }}
+                  >
+                    Intas Eazypet
+                  </Text>
+                  <Text
+                    style={{
+                      color: "#B9C4CF",
+                      fontSize: 11,
+                      fontWeightL: "400",
+                    }}
+                  >
+                    After Breakfast
+                  </Text>
+                </View>
+
+                <Text
+                  style={{
+                    marginLeft: 100,
+                    color: "#41CE8A",
+                    fontSize: 18,
+                    fontWeight: "700",
+                  }}
+                >
+                  1 No
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
 
         {todayReminders.length > 0 && (
           <>
