a600e1ccf869fa6d39a3b4cedf313d6da7d051fb
Tejendra Singh <singhtej9936@gmail.com>
2021-07-01 11:08:18 +0530

    initial commit

--- app/screens/ChatScreen.js
+++ app/screens/ChatScreen.js
@@ -93,6 +93,12 @@ const ChatScreen = ({ navigation, route, onChangeImage }) => {
       const results = await DocumentPicker.pickMultiple({
         type: [DocumentPicker.types.allFiles],
       });
+      console.log(
+        res.uri,
+        res.type, // mime type
+        res.name,
+        res.size
+      );
       for (const res of results) {
         console.log(res.uri, res.type, res.name, res.size);
       }
